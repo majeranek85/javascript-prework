6ad65df3cb6a999ad0d3386ec8fb0d6b3ab7b9b9
Marta Majerowska <majeranek85@gmail.com>
2019-12-04 19:37:52 +0100

    Add condition for players invalid choice

--- js/script.js
+++ js/script.js
@@ -28,19 +28,18 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let argPlayerMove = getMoveName(playerInput);
 
-
 // Result
 
 function displayResult(argComputerMove, argPlayerMove) {
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 
-  if (argComputerMove == "kamień" && argPlayerMove == "papier" || argComputerMove == 'papier' && argPlayerMove == 'nożyce' || argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+  if (argComputerMove == 'kamień' && argPlayerMove == 'papier' || argComputerMove == 'papier' && argPlayerMove == 'nożyce' || argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
     printMessage('Ty wygrywasz!');
-  }
-  else if (argComputerMove == argPlayerMove) {
+  } else if (argComputerMove == argPlayerMove) {
     printMessage('Remis!');
-  }
-  else {
+  } else if (argPlayerMove == 'nieznany ruch') {
+    printMessage('Nie wybrałeś poprawnego ruchu! Wybierz: 1-papier, 2-kamień lub 3-nożyce.')
+  } else {
     printMessage('Ty przegrywasz :(');
   }
 }
