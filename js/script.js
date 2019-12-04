35f46c87c251e063d7a9668d2fdf9d71551d6a6d
Marta Majerowska <majeranek85@gmail.com>
2019-12-04 13:05:55 +0100

    Fix if else condition for computer and player choice

--- js/script.js
+++ js/script.js
@@ -6,19 +6,17 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
-if(randomNumber == 1){
+if (randomNumber == 1){
   computerMove = 'kamień';
-}
-
-if(randomNumber == 2){
+} else if (randomNumber == 2){
   computerMove = 'papier';
-}
-
-if(randomNumber == 3){
+} else if (randomNumber == 3){
   computerMove = 'nożyce';
+} else {
+  computerMove;
 }
 
-printMessage('Mój ruch to: ' + computerMove)
+printMessage ('Mój ruch to: ' + computerMove)
 
 //Players move
 
@@ -28,16 +26,14 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
+if (playerInput == '1'){
   playerMove = 'kamień';
-}
-
-if(playerInput == '2'){
+} else if (playerInput == '2'){
   playerMove = 'papier';
-}
-
-if(playerInput == '3'){
+} else if (playerInput == '3'){
   playerMove = 'nożyce';
+} else {
+  playerMove;
 }
 
 printMessage('Twój ruch to: ' + playerMove);
